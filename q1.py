e2c779804a396fa12c2fbd9d448ac1a5b0a84909
g-laz77 <suhanprabhu@gmail.com>
2017-08-16 18:30:45 +0530

    added code for graph generation of first random walk

---
+++ q1.py
@@ -0,0 +1,51 @@
+import random
+import matplotlib.pyplot as py
+# import plotly.graph_objs as go
+N = list(range(0, 100))
+probs = []
+# Create random data with numpy
+import numpy as np
+for lo in range(100):
+    origin = 0
+    for i in range(5000):
+        one_x = 0
+        two_x = 0
+        
+
+        for j in range(0,lo):
+            temp = random.uniform(0,1)
+            if temp > 0.5:
+                one_x += 1
+            else:
+                one_x -= 1
+
+        for j in range(0,lo):
+            temp1 = random.uniform(0,1)
+            if temp1 > 0.5:
+                two_x += 1
+            else:
+                two_x -= 1
+    
+        if one_x == two_x:
+            origin += 1
+
+    probability = float(origin)/5000
+    print(probability)
+    probs.append(probability)
+
+py.plot(N,probs)
+# N = 500
+# random_x = np.linspace(0, 1, N)
+# random_y = np.random.randn(N)
+
+# Create a trace
+# trace = go.Scatter(
+#     x = N,
+#     y = probs
+# )
+
+# data = [trace]
+
+# py.iplot(data, filename='basic-line')
+
+py.show()
